dc2a2837ba87b3ec7cfb63d10b661ddba1a3f721
phudlow <patrick.hudlow@gmail.com>
2026-07-01 11:23:01 -0800

    Added vitest for unit testing & example test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    include: ["./src/*.test.ts"],
+  },
 });
